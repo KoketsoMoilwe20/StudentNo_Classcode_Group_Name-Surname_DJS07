f14fd90411f7c77b0f0b2843541974ad7e94f954
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-31 10:49:55 +0200

    Add: attributes for styling

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,9 +1,22 @@
 export default function Meme() {
     return (
-        <form>
-            <input></input>
-            <input></input>
-            <button>Get a new meme image</button>
-        </form>
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                >Get a new meme image</button>
+            </form>
+        </main>
+        
     )
 }
