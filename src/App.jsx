411178fc81b6c0f95b1f44f8beea426017c8105b
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-31 10:42:27 +0200

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React from "react"
-import Header from "./Header"
-
+import Header from "./Header.jsx";
 
 export default function App() {
-
   return (
-    <>
+    <div>
       <Header />
-    </>
-  )
+    </div>
+  );
 }
-
-
