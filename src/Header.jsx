30cd0125f688538df1cabedc5b27316e3217f8dc
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-31 10:42:10 +0200

    Add image

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <nav className="header">
+      <img src="../images/troll-face.png" className="header--image" />
+      <h2 className="header--title">Header component</h2>
+      <h4 className="header--project"> React Course - Project 3</h4>
+    </nav>
+  );
+}
